29692b9fcdfa87e1fc910795f8b76afbf6b9a259
Axlii50 <kubad4d@wp.pl>
2023-07-30 22:43:29 +0200

    Bugfix

--- RefakturyzacjaTwilo/Form1.cs
+++ RefakturyzacjaTwilo/Form1.cs
@@ -6,64 +6,62 @@ using System.IO;
 
 namespace RefakturyzacjaTwilo
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 1;
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            DateTime input = new DateTime(this.dateTimePicker1.Value.Ticks, DateTimeKind.Utc);
 
-		private void Form1_Load(object sender, EventArgs e)
-		{
-			comboBox1.SelectedIndex = 1;
-		}
-		private async void button1_Click(object sender, EventArgs e)
-		{
-			DateTime input = this.dateTimePicker1.Value;
-			//DateTime input2 = new DateTime(this.dateTimePicker1.Value.Ticks, DateTimeKind.Utc);
+            //download orders from given date
+            List<CheckOutForm> Orders = await Program._allegroApi.GetOrders(input, OrderStatusType.SENT);
+            Orders.AddRange(await Program._allegroApi.GetOrders(input, OrderStatusType.PICKED_UP));
 
-			//download orders from given date
-			List<CheckOutForm> Orders = await Program._allegroApi.GetOrders(input, OrderStatusType.PICKED_UP);
-			//List<CheckOutForm> Orders = await Program._allegroApi.GetOrders(input, OrderStatusType.SENT);
+            string length = Orders.Count.ToString();
+            label2.Text = length;
 
-			string length = Orders.Count.ToString();
-			label2.Text = length;
+            string timestamp = DateTime.Now.ToString("--yyyy-MM-dd--HH-mm-ss");
+            string path = @"Orders\orders" + timestamp + ".txt";
 
-			string timestamp = DateTime.Now.ToString("--yyyy-MM-dd--HH-mm-ss");
-			string path = @"Orders\orders" + timestamp + ".txt";
+            string content = string.Empty;
+            foreach (var order in Orders)
+            {
+                foreach (var item in order.lineItems)
+                {
+                    content += item.offer.name;
+                    content += "\t";
+                    content += item.originalPrice.amount;
+                    content += "\t";
+                    content += item.boughtAt;
+                    content += "\t";
+                    content += item.offer.external?.id;
+                    content += '\n';
+                }
+            }
+            System.Diagnostics.Debug.WriteLine(content);
 
-			string content = string.Empty;
-			foreach (var order in Orders)
-			{
-				foreach (var item in order.lineItems)
-				{
-					content += item.offer.name;
-					content += ";";
-					content += item.originalPrice;
-					content += ";";
-					content += item.boughtAt;
-					content += ";";
-					content += item.offer.external;
-					content += '\n';
-				}
-			}
-			System.Diagnostics.Debug.WriteLine(content);
+            File.WriteAllText(path, content);
 
-			File.WriteAllText(path, content);
+            string dirPath = Path.Combine(Directory.GetCurrentDirectory(), "Orders");
+            label3.Visible = true;
+            label4.Text = "orders" + timestamp + ".txt";
+            label4.Visible = true;
+            label5.Visible = true;
+            linkLabel1.Text = dirPath;
+            linkLabel1.Visible = true;
+        }
 
-			string dirPath = Path.Combine(Directory.GetCurrentDirectory(), @"Orders");
-			label3.Visible = true;
-			label4.Text = "orders" + timestamp + ".txt";
-			label4.Visible = true;
-			label5.Visible = true;
-			linkLabel1.Text = dirPath;
-			linkLabel1.Visible = true;
-		}
-
-		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-		{
-			Process.Start("explorer.exe", @"Orders");
-		}
-	}
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("explorer.exe", "Orders");
+        }
+    }
 }
